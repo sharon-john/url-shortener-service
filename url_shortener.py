094ea6c4346766ed3e1793b0591b9ac4022813a4
Sharon John <sharonbinujohn@gmail.com>
2025-03-18 15:48:54 -0700

    new short URL generation scheme and UTs for it and collision proofing

--- url_shortener.py
+++ url_shortener.py
@@ -3,7 +3,8 @@ import hashlib
 from urllib.parse import urlparse, parse_qsl, urlencode, urlunparse
 
 class URLShortener:
-    # salt to make it so that short URLs cannot be guessed from their long URLs 
+    # salt to make it so that short URLs cannot be guessed from their long URLs even with knowledge of the hashing scheme 
+    # using a hardcoded string not dependent on random properties for deterministic generation of URLs
     salt = "randomstring"
 
     def __init__(self, default_domain=""):
@@ -18,22 +19,32 @@ class URLShortener:
         return self.default_domain
 
     """
-    Generates a 6 character-length base62 short code that represents the path in a URL
+    Generates a 6 character-length base64 encoded short code that represents the path in a URL
     """
     def _generate_short_code(self, url):
         if url not in self.long_url_mapping:
             # md5 hash the URL and salt 
             string_to_hash = url + URLShortener.salt 
-            hash = hashlib.md5(string_to_hash.encode("utf-8")).hexdigest()
-            # encode the hashed contents to url safe base64 
-            bytes = base64.urlsafe_b64encode(hash.encode("utf-8"))
+            hash = hashlib.md5(string_to_hash.encode("utf-8")).digest()
+            # encode the hash to url safe base64 
+            bytes = base64.urlsafe_b64encode(hash)
             # finally truncate the result and choose the first 6 characters
             return bytes.decode('utf-8')[0:6]
     
-    """
-    Generates a new short URL if it's a new long url or fetches an existing short URL
-    """
-    def shorten_url(self, long_url):
+    def _get_short_url(self, long_url):
+        short_code = ""
+        attempts = 0
+        while (len(short_code) < 6 or (short_url in self.short_url_mapping)):
+            # if we encountered a collision, add a counter to the end of the URL and retry 
+            if attempts > 0:
+                long_url = long_url + str(attempts)
+            short_code = self._generate_short_code(long_url)
+            short_url = self.default_domain.rstrip("/") + "/" + short_code
+            attempts += 1 
+
+        return short_url
+
+    def _parse_long_url(self, long_url):
         # default scheme to http and allow fragments incase user wants to link two parts of a p
         parsed_long_url = urlparse(long_url, scheme="http", allow_fragments=True)
 
@@ -46,6 +57,14 @@ class URLShortener:
             raise Exception("Scheme not supported by URL shortener service")
         if len(domain) < 1 or len(domain) > 255:
             raise Exception("Domain is longer than RFC permits")
+
+        return parsed_long_url, scheme, domain
+    
+    """
+    Generates a new short URL if it's a new long url or fetches an existing short URL
+    """
+    def shorten_url(self, long_url):
+        parsed_long_url, scheme, domain = self._parse_long_url(long_url)
                             
         # query parameters should be sorted to preserve uniqueness across differently ordered but identical query parameters 
         sorted_query_params = urlencode(sorted(parse_qsl(parsed_long_url.query))) if parsed_long_url.query else ""
@@ -64,16 +83,7 @@ class URLShortener:
         if long_url in self.long_url_mapping:
             return False, self.long_url_mapping[long_url]
 
-        short_code = ""
-        num_runs = 0
-        while (len(short_code) < 6 or short_code in self.short_url_mapping):
-            # if we encountered a collision, add a counter to the end of the URL and retry 
-            if num_runs > 0:
-                long_url = long_url + str(num_runs)
-            short_code = self._generate_short_code(long_url)
-            num_runs += 1 
-
-        short_url = self.default_domain.rstrip("/") + "/" + short_code
+        short_url = self._get_short_url(long_url)
 
         self.long_url_mapping[long_url] = short_url 
         # store the short url to its underlying long URL
--- url_shortener_test.py
+++ url_shortener_test.py
@@ -3,15 +3,17 @@ from urllib.parse import urlparse
 import url_shortener 
 
 class URLShortenerTest(unittest.TestCase):
+    domain = "actuate.ai"
+    short_url_path_size = 6
+
     def setUp(self):
-        self.domain = "actuate.ai"
-        self.url_shortener = url_shortener.URLShortener(self.domain)
+        self.url_shortener = url_shortener.URLShortener(URLShortenerTest.domain)
 
     """Test if the shortened URL's domain matches the expected domain"""
     def test_domain_equals(self):
         actual_url = "https://actuate.ai/somelongpath"  # Dummy URL for testing
         parsed_url = urlparse(actual_url)
-        expected = self.domain  # Expected domain
+        expected = URLShortenerTest.domain  # Expected domain
         self.assertEqual(expected, parsed_url.netloc)
 
     """Test the URL shortening functionality"""
@@ -25,10 +27,10 @@ class URLShortenerTest(unittest.TestCase):
         parsed_url = urlparse(short_url)
         domain_and_path =  parsed_url.path.split("/")
         # The domain in the shortened URL should match
-        self.assertEqual(self.domain, domain_and_path[0])
+        self.assertEqual(URLShortenerTest.domain, domain_and_path[0])
 
-        # The path should be 6 characters long
-        self.assertEqual(6, len(domain_and_path[1]))
+        # The path should be URLShortener.short_url_path_size characters long
+        self.assertEqual(URLShortenerTest.short_url_path_size, len(domain_and_path[1]))
 
         # Shortening the same URL should return the same short URL
         shortened_now2, short_url2 = self.url_shortener.shorten_url(long_url)
@@ -41,6 +43,7 @@ class URLShortenerTest(unittest.TestCase):
         self.assertFalse(shortened_now3)
         self.assertEqual(short_url, short_url3)
     
+    """Test that reordered but identical query parameters are considered the same"""
     def test_shorten_url_query_params(self):
         # query params with different orders 
         long_url_with_query_params = "https://www.example.com/hello/ok/longurl/here?query1=ok&query2=3&query3=hi"
@@ -50,16 +53,17 @@ class URLShortenerTest(unittest.TestCase):
         parsed_url = urlparse(short_url)
         domain_and_path =  parsed_url.path.split("/")
         # The domain in the shortened URL should match
-        self.assertEqual(self.domain, domain_and_path[0])
+        self.assertEqual(URLShortenerTest.domain, domain_and_path[0])
 
         # The path should be 6 characters long
-        self.assertEqual(6, len(domain_and_path[1]))
+        self.assertEqual(URLShortenerTest.short_url_path_size, len(domain_and_path[1]))
 
         long_url_with_query_params_v2 = "hTTps://wwW.example.com/hello/ok/longurl/here?query2=3&query1=ok&query3=hi"
         shortened_now2, short_url2 = self.url_shortener.shorten_url(long_url_with_query_params_v2)
         self.assertFalse(shortened_now2)
         self.assertEqual(short_url, short_url2)
     
+    """ Test that the shortening works on the same domain itself"""
     def test_shorten_url_same_domain(self):
         long_url_same_domain = "https://actuate.ai/long/path/on/same/domain"
         shortened_now, short_url = self.url_shortener.shorten_url(long_url_same_domain)
@@ -68,11 +72,62 @@ class URLShortenerTest(unittest.TestCase):
         parsed_url = urlparse(short_url)
         domain_and_path =  parsed_url.path.split("/")
         # The domain in the shortened URL should match
-        self.assertEqual(self.domain, domain_and_path[0])
+        self.assertEqual(URLShortenerTest.domain, domain_and_path[0])
 
-        # The path should be 6 characters long
-        self.assertEqual(6, len(domain_and_path[1]))
+        # The path should be URLShortener.short_url_path_size characters long
+        self.assertEqual(URLShortenerTest.short_url_path_size, len(domain_and_path[1]))
+    
+    """ The same long URL should always produce the same short URL deterministically """
+    def test_shorten_url_same_url_across_instances(self):
+        long_url_same_domain = "https://actuate.ai/long/path/on/same/domain"
+        shortened_now, short_url = self.url_shortener.shorten_url(long_url_same_domain)
+        self.assertTrue(shortened_now)
 
+        # new instance 
+        url_shortener2 = url_shortener.URLShortener(URLShortenerTest.domain)
+        shortened_now2, short_url2 = url_shortener2.shorten_url(long_url_same_domain)
+
+        self.assertTrue(shortened_now2)
+        # The same long URL should produce the same shortened URL always 
+        self.assertEqual(short_url, short_url2)
+    
+    """Test that in the unlikely event of a collision (short code already exists), we are able to produce a new valid short code"""
+    def test_shorten_url_collisions(self):
+        long_url_same_domain = "https://actuate.ai/long/path/on/same/domain"
+        short_url_for_this = "actuate.ai/fJZlUx"
+
+        # simulating a hash collision
+        self.url_shortener.short_url_mapping[short_url_for_this] = ["https://example.com/ok", 0]
+        
+        # since we have a collision, we expect url shortener to append a count to it and produce a new URL
+        shortened_now, short_url = self.url_shortener.shorten_url(long_url_same_domain)
+        self.assertTrue(shortened_now)
+        self.assertNotEqual(short_url_for_this, short_url)
+
+        parsed_url = urlparse(short_url)
+        domain_and_path =  parsed_url.path.split("/")
+        self.assertEqual(URLShortenerTest.short_url_path_size, len(domain_and_path[1]))
+
+        # now claim this short code as well and see if increment to 2 works
+        del self.url_shortener.short_url_mapping[short_url]
+        del self.url_shortener.long_url_mapping[long_url_same_domain]
+
+        # jerry rig the second short code
+        short_url_attempt2 = "actuate.ai/" + domain_and_path[1]
+        self.url_shortener.short_url_mapping[short_url_attempt2] = ["https://example.com/ok2", 0]
+
+        # now we will have 2 collisions when we attempt to generate this short code, we will ultimately produce the short URL for https://actuate.ai/long/path/on/same/domain2
+        shortened_now2, short_url2 = self.url_shortener.shorten_url(long_url_same_domain)
+        self.assertTrue(shortened_now2)
+        self.assertNotEqual(short_url_for_this, short_url2)
+
+        parsed_url = urlparse(short_url2)
+        domain_and_path =  parsed_url.path.split("/")
+        self.assertEqual(URLShortenerTest.short_url_path_size, len(domain_and_path[1]))
+
+        self.assertNotEqual(short_url, short_url2)
+
+        print(short_url, short_url2)
         
 if __name__ == '__main__':
     unittest.main()
